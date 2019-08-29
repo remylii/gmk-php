83d723ec27f8bec1e214a2964d1168b16c2eef35
remyli <rapi.remyli@gmail.com>
2019-08-29 13:31:17 +0900

    道しるべを設定

--- tests/GameManagerTest.php
+++ tests/GameManagerTest.php
@@ -8,13 +8,8 @@ class GameManagerTest extends TestCase
 {
     public function testStart()
     {
-        ob_start();
-
-        $gm = new GameManager();
-        $gm->start();
-
-        $output_contents = ob_get_clean();
-
-        $this->assertEquals("GAME START!\n", $output_contents);
+        $this->markTestIncomplete(
+            'このテストは、まだ実装されていません。'
+        );
     }
 }
