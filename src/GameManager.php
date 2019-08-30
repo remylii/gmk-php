e410034299485e44127ac729671cbbc8c2700a6d
remyli <rapi.remyli@gmail.com>
2019-08-30 12:57:01 +0900

    黒白をそれぞれ交互にターンでおけるように

--- src/GameManager.php
+++ src/GameManager.php
@@ -15,25 +15,44 @@ class GameManager
     public function start()
     {
         /** 登場人物を整理させる */
-        // 盤面, 置く石, プレイヤー
 
         /** ゲームのシナリオを整理させる */
+        $stones = ["●", "○"];
         while (true) {
-            // 1. ボード表示
-            $this->board->show();
+            foreach ($stones as $stone) {
+                echo $stone . "のターン" . PHP_EOL;
+                // 1. ボード表示
+                $this->board->show();
 
-            // 2. 色ぬり
-            echo "このターンのプレイヤーが石を置く\n";
+                // 2. 色ぬり
+                echo "座標を「x,y」の形で入力してください..." . PHP_EOL;
+                while(true) {
+                    try {
+                        $stdin = trim(fgets(STDIN));
+                        list($x, $y) = $this->parseArgsXY($stdin);
+                        $this->board->put($stone, $x, $y);
+                    } catch (\InvalidArgumentException | \LogicException $e) {
+                        echo $e->getMessage() . PHP_EOL;
+                        continue;
+                    }
 
-            // 3. 勝敗判定
-            echo "勝敗が決したか確認する\n";
+                    break 1;
+                }
 
-            // 4. 相手のターン
-            echo "勝負がついていなければ相手のターン\n";
+                // 3. 勝敗判定
 
+                // 4. 相手のターン
+            }
             // 5. 繰り返し
-
-            return;
         }
+    }
+
+    private function parseArgsXY(string $str)
+    {
+        if (!preg_match("/^(\d+),(\d+)$/", $str, $res)) {
+            throw new \InvalidArgumentException('入力の形が違う');
+        }
+
+        return [$res[1], $res[2]];
     }
 }
